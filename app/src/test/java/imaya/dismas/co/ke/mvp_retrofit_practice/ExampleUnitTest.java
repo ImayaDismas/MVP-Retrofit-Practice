df25bff32f582d7703afa5d40917facbdd890ba8
Imaya <dismas@nostra.co.ke>
2019-07-29 09:58:21 +0300

    initialized and added the dependencies to the project

---
+++ app/src/test/java/imaya/dismas/co/ke/mvp_retrofit_practice/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package imaya.dismas.co.ke.mvp_retrofit_practice;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
